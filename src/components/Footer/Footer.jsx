d5594157f4cae6f05cae9354622565eb939281f0
alikzilla <nuspekovalihan@gmail.com>
2024-04-15 12:32:20 +0500

    fixed logic of mst

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,17 @@ import styles from './footer.module.css';
 const Footer = ({}) => {
   return (
     <footer className={styles.footer}>
-      <a href="" target='_blank'>telegram</a>
+      <a href="" className={styles.link}><p>Alikhan</p></a>
+      <p>-</p>
+      <a href="" className={styles.link}><p>Zhartas</p></a>
+      <p>-</p>
+      <a href="" className={styles.link}><p>Shyntemir</p></a>
+      <p>-</p>
+      <a href="" className={styles.link}><p>Almaz</p></a>
+      <p>-</p>
+      <a href="" className={styles.link}><p>Temirlan</p></a>
+      <p>-</p>
+      <a href="" className={styles.link}><p>Adil</p></a>
     </footer>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,9 +4,6 @@ import Graph from '../Graph/Graph'
 const Main = ({}) => {
   return (
     <section className={styles.graphBlock}>
-      <div className={styles.content}>
-        <h1>Kruskal's Algorihm in MST(Minimum Spanning Tree)</h1>
-      </div>
       <Graph />
     </section>
   )
