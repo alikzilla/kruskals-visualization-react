3441abee971e14b2e65636eed68e910814e84b6d
alikzilla <nuspekovalihan@gmail.com>
2024-04-15 23:31:07 +0500

    exluded chance of infitiy loop, added new ready graphs

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -29,15 +29,62 @@ function GraphVisualization() {
     height: "500px",
   };
 
+  const predefinedGraphs = [
+    {
+      nodes: [
+        { id: 1, label: "1" },
+        { id: 2, label: "2" },
+        { id: 3, label: "3" },
+        { id: 4, label: "4" },
+        { id: 5, label: "5" },
+      ],
+      edges: [
+        { from: 1, to: 2, label: "3" },
+        { from: 1, to: 3, label: "4" },
+        { from: 1, to: 4, label: "1" },
+        { from: 1, to: 5, label: "5" },
+        { from: 2, to: 3, label: "8" },
+        { from: 2, to: 4, label: "6" },
+        { from: 2, to: 5, label: "9" },
+        { from: 3, to: 4, label: "10"},
+        { from: 3, to: 5, label: "2" },
+        { from: 4, to: 5, label: "13"},
+      ],
+    },
+    {
+      nodes: [
+        { id: 1, label: "1", x: 0, y: 0 },
+        { id: 2, label: "2", x: 1, y: 0 },
+        { id: 3, label: "3", x: 2, y: 0 },
+        { id: 4, label: "4", x: 0, y: 1 },
+        { id: 5, label: "5", x: 1, y: 1 },
+        { id: 6, label: "6", x: 2, y: 1 },
+        { id: 7, label: "7", x: 1, y: 2 },
+      ],
+      edges: [
+        { from: 1, to: 2, label: "3" },
+        { from: 2, to: 3, label: "2" },
+        { from: 1, to: 4, label: "1" },
+        { from: 2, to: 5, label: "6" },
+        { from: 3, to: 6, label: "7" },
+        { from: 4, to: 5, label: "4" },
+        { from: 5, to: 6, label: "9" },
+        { from: 4, to: 7, label: "3" },
+        { from: 5, to: 7, label: "10"},
+        { from: 6, to: 7, label: "1" },
+      ]
+    }
+  ];
+
   function generateRandomGraph() {
-    const numNodes = Math.floor(Math.random() * 10) + 3; // Random number of nodes between 5 and 14
+    const numNodes = Math.floor(Math.random() * 10) + 3;
     const nodes = [];
     for (let i = 1; i <= numNodes; i++) {
       nodes.push({ id: i, label: `${i}`, title: `node ${i} tooltip text` });
     }
 
     const edges = [];
-    const visitedEdges = new Set(); // Keep track of visited edges
+    const visitedEdges = new Set();
 
     for (let i = 1; i <= numNodes; i++) {
       const from = i;
@@ -45,38 +92,35 @@ function GraphVisualization() {
       do {
         to = Math.floor(Math.random() * numNodes) + 1;
       } while (
-        to === from ||
-        visitedEdges.has(`${from}-${to}`) ||
-        visitedEdges.has(`${to}-${from}`)
+        visitedEdges.has(`${from}-${to}`) || visitedEdges.has(`${to}-${from}`)
       );
 
       visitedEdges.add(`${from}-${to}`);
       visitedEdges.add(`${to}-${from}`);
 
-      const weight = Math.floor(Math.random() * 10) + 1; // Random weight between 1 and 10
-      edges.push({ from, to, label: `${weight}`, length: weight * 30 }); // Adding edge from A to B
-      edges.push({ from: to, to: from, label: `${weight}` }); // Adding edge from B to A (undirected)
+      const weight = Math.floor(Math.random() * 10) + 1;
+      edges.push({ from, to, label: `${weight}`, length: weight * 30 });
+      edges.push({ from: to, to: from, label: `${weight}` });
     }
 
     return { nodes, edges };
   }
 
   function findMST() {
-    computeMSTUsingKruskal(graph, setGraph); // Pass setGraph
+    computeMSTUsingKruskal(graph, setGraph);
   }
 
   function computeMSTUsingKruskal(graph, setGraph) {
     const sortedEdges = graph.edges.slice().sort((a, b) => {
-      return parseInt(a.label) - parseInt(b.label); // Sort edges by weight
+      return parseInt(a.label) - parseInt(b.label);
     });
 
-    console.log(sortedEdges.length)
+    console.log(sortedEdges.length);
 
     const mstEdges = [];
     const disjointSets = new Map();
-    const processedEdges = new Set(); // Keep track of processed edges
+    const processedEdges = new Set();
 
-    // Initialize disjoint sets
     for (let node of graph.nodes) {
       disjointSets.set(node.id, node.id);
     }
@@ -105,13 +149,12 @@ function GraphVisualization() {
     const interval = setInterval(() => {
       if (index >= sortedEdges.length) {
         clearInterval(interval);
-        console.log("cleared")
+        console.log("cleared");
         return;
       }
 
       const edge = sortedEdges[index];
 
-      // Check if the edge has been processed already
       if (!processedEdges.has(`${edge.from}-${edge.to}`)) {
         setGraph((prevGraph) => ({
           ...prevGraph,
@@ -144,10 +187,11 @@ function GraphVisualization() {
         processedEdges.add(`${edge.to}-${edge.from}`);
       }
 
-      console.log(index)
+      console.log(index);
       index++;
     }, 1000);
 
+    console.log(disjointSets);
   }
 
   function resetGraph() {
@@ -155,33 +199,49 @@ function GraphVisualization() {
     setGraph(newGraph);
   }
 
+  function setPredefinedGraph(index) {
+    setGraph(predefinedGraphs[index]);
+  }
+
   return (
     <section className={styles.graphVisualization}>
-      <div className={styles.graphs}>
-        <div className={styles.initialTree}>
-          <h1>Initial Tree</h1>
-          <section className={styles.graph}>
-            <Graph
-              graph={graph}
-              className={styles.graph}
-              options={optionsGraph}
-              events={{
-                select: function (event) {
-                  console.log("Node selected:", event.nodes);
-                },
-              }}
-            />
+      <div className={styles.wrapper}>
+        <div className={styles.buttons}>
+          <button className={styles.button} onClick={resetGraph}>
+            New Graph
+          </button>
+          <button className={styles.button} onClick={() => setPredefinedGraph(0)}>
+            K5
+          </button>
+          <button className={styles.button} onClick={() => setPredefinedGraph(1)}>
+            Tessellation
+          </button>
+        </div>
+        <div className={styles.graphs}>
+          <div className={styles.initialTree}>
+            <h1>Initial Tree</h1>
+            <section className={styles.graph}>
+              <Graph
+                graph={graph}
+                className={styles.graph}
+                options={optionsGraph}
+                events={{
+                  select: function (event) {
+                    console.log("Node selected:", event.nodes);
+                  },
+                }}
+              />
+            </section>
+          </div>
+          <section className={styles.controls}>
+            <button className={styles.button} onClick={findMST}>
+              Find MST
+            </button>
           </section>
         </div>
       </div>
-      <section className={styles.controls}>
-        <button className={styles.button} onClick={resetGraph}>
-          New Graph
-        </button>
-        <button className={styles.button} onClick={findMST}>
-          Find MST
-        </button>
-      </section>
+      
+      
     </section>
   );
 }
